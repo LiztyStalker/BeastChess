3435dd22245a52619eedb7a2d3f2f04a0df4f9a7
LiztyStalker <31952112+LiztyStalker@users.noreply.github.com>
2021-12-02 00:11:40 +0900

    NEW : CommandTest 임시 제작

--- Assets/Test/PlayMode/PlayTest.cs
+++ Assets/Test/PlayMode/PlayTest.cs
@@ -55,13 +55,93 @@ public class PlayTest
     }
 
     [UnityTest]
-    public IEnumerator BattieField_Created()
+    public IEnumerator BattleField_Created()
     {
         yield return null;
         Assert.NotNull(battleFieldManager);
     }
 
+    [UnityTest]
+    public IEnumerator BattleField_AllCommandTest()
+    {
+        yield return BattleCommandTest(true);
+    }
 
+    [UnityTest]
+    public IEnumerator BattleField_RandomCommandTest()
+    {
+        yield return BattleCommandTest(false);
+    }
+
+    public IEnumerator BattleCommandTest(bool isLinear)
+    {
+        var leftData = DataStorage.Instance.GetDataOrNull<UnitData>("Conscript");
+        var rightData = DataStorage.Instance.GetDataOrNull<UnitData>("Conscript");
+
+        Debug.Log($"Test Start {leftData.name} - {rightData.name}");
+
+        var blocksL = FieldManager.GetTeamUnitBlocksFromVertical(TYPE_BATTLE_TEAM.Left);
+        var blocksR = FieldManager.GetTeamUnitBlocksFromVertical(TYPE_BATTLE_TEAM.Right);
+
+        for (int i = 0; i < blocksL.Length; i++)
+        {
+            var uCardL = UnitCard.Create(leftData);
+            unitManager.CreateUnit(uCardL, uCardL.UnitKeys[0], blocksL[i], TYPE_BATTLE_TEAM.Left);
+        }
+        yield return null;
+
+        for (int i = 0; i < blocksR.Length; i++)
+        {
+            var uCardR = UnitCard.Create(rightData);
+            unitManager.CreateUnit(uCardR, uCardR.UnitKeys[0], blocksR[i], TYPE_BATTLE_TEAM.Right);
+        }
+        yield return null;
+
+        var costRateL = (42f / (float)leftData.SquadCount) * (float)leftData.AppearCostValue;
+        var costRateR = (42f / (float)rightData.SquadCount) * (float)rightData.AppearCostValue;
+
+        Debug.Log($"[CountL {FieldManager.IsHasTeamUnitActorCount(TYPE_BATTLE_TEAM.Left)}  Rate {costRateL}] - [CountR {FieldManager.IsHasTeamUnitActorCount(TYPE_BATTLE_TEAM.Right)}  Rate {costRateR}]");
+
+        //Assert.That(FieldManager.IsHasTeamUnitActorCount(TYPE_BATTLE_TEAM.Left) == 42, Is.True);
+        //Assert.That(FieldManager.IsHasTeamUnitActorCount(TYPE_BATTLE_TEAM.Right) == 42, Is.True);
+        yield return null;
+
+        int turn = 10;
+        TYPE_BATTLE_TURN[] _turns = new TYPE_BATTLE_TURN[turn];
+
+        if (isLinear)
+        {
+            for(int i = 0; i < turn; i++)
+            {
+                _turns[i] = (TYPE_BATTLE_TURN)(i % System.Enum.GetValues(typeof(TYPE_BATTLE_TURN)).Length);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < turn; i++)
+            {
+                _turns[i] = (TYPE_BATTLE_TURN)UnityEngine.Random.Range(0, (int)TYPE_BATTLE_TURN.Backward + 1);
+            }
+        }
+
+
+        while (!unitManager.IsLiveUnitsEmpty())
+        {
+            Debug.Log(_turns[turn - 1]);
+            battleFieldManager.NextTurnTester(_turns[turn - 1], _turns[turn - 1]);
+
+            while (battleFieldManager.isRunning)
+            {
+                yield return null;
+            }
+            turn--;
+            if (turn == 0)
+                break;
+        }
+
+        Debug.Log(unitManager.BattleResultToString());
+        Debug.Log("TestEnd");
+    }
 
 }
 #endif
